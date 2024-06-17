a84a3c68d0e93bc5c848004fe1f840d27864a950
Khushal Agarwal <khushal.agarwal987@gmail.com>
2024-06-17 21:27:44 +0530

    feat: added bug page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,59 +1,129 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, Button } from "react-native";
+import ImagePicker, {
+  getCameraPermissionsAsync,
+  getMediaLibraryPermissionsAsync,
+  ImagePickerAsset,
+  launchCameraAsync,
+  launchImageLibraryAsync,
+  requestCameraPermissionsAsync,
+  requestMediaLibraryPermissionsAsync,
+} from "expo-image-picker";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { useState } from "react";
+import axios from "axios";
 
 export default function HomeScreen() {
+  const [photo, setPhoto] = useState<ImagePickerAsset | null>(null);
+
+  const openImagePicker = async () => {
+    const permissionCheck = await getMediaLibraryPermissionsAsync();
+    const canRequest = permissionCheck.canAskAgain;
+    let permissionGranted = permissionCheck.granted;
+    if (!permissionGranted) {
+      if (canRequest) {
+        const response = await requestMediaLibraryPermissionsAsync();
+        permissionGranted = response.granted;
+      } else {
+        return { askToOpenSettings: true, cancelled: true };
+      }
+    }
+    if (permissionGranted) {
+      const imagePickerSuccessResult = await launchImageLibraryAsync();
+      const assets = imagePickerSuccessResult.assets;
+      // since we only support single photo upload for now we will only be focusing on 0'th element.
+      const photo = assets && assets[0];
+      setPhoto(photo);
+    }
+  };
+
+  const openCameraPicker = async () => {
+    const permissionCheck = await getCameraPermissionsAsync();
+    const canRequest = permissionCheck.canAskAgain;
+    let permissionGranted = permissionCheck.granted;
+    if (!permissionGranted) {
+      if (canRequest) {
+        const response = await requestCameraPermissionsAsync();
+        permissionGranted = response.granted;
+      } else {
+        return { askToOpenSettings: true, cancelled: true };
+      }
+    }
+    if (permissionGranted) {
+      const imagePickerSuccessResult = await launchCameraAsync();
+      const assets = imagePickerSuccessResult.assets;
+      // since we only support single photo upload for now we will only be focusing on 0'th element.
+      const photo = assets && assets[0];
+      setPhoto(photo);
+    }
+  };
+
+  // {"assetId": null, "base64": null, "duration": null, "exif": null,
+  // "fileName": "6ff93bdd-ff8c-40b1-9bd9-f8a8a02e65e2.jpeg", "fileSize": 187799, "height": 1280,
+  // "mimeType": "image/jpeg", "rotation": null, "type": "image",
+  // "uri": "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252Fexpo-51-image-upload-bug-80c9d27e-97e1-44c9-ab10-4b8cade47bb5/ImagePicker/5d4d3fc2-5fc3-4a18-a4a3-03c4b1ea992f.jpeg", "width": 960}
+  const uploadPhoto = async () => {
+    if (photo) {
+      const uri = photo.uri;
+      const filename = photo.fileName;
+      const type = photo.mimeType;
+      const formData = new FormData();
+      formData.append("image", {
+        uri,
+        name: filename,
+        type,
+      } as any);
+
+      try {
+        const { data } = await axios.post(
+          `https://chat.stream-io-api.com/channels/messaging/c3840f01-a54c-4563-9bb1-2d220a89d83c/image?user_id=khushalagarwalgetstreamio&connection_id=65e9ec5e-0a09-243b-0000-00000219d991&api_key=v3weqm7meazf`,
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+              "Stream-Auth-Type": "jwt",
+              Authorization:
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoia2h1c2hhbGFnYXJ3YWxnZXRzdHJlYW1pbyJ9.8Pw06BSDDk1BLiho10CYTvtglUH00rakVhGpkFDt-NE",
+            },
+          }
+        );
+        if (!data.isSuccess) {
+          alert("Image upload failed!");
+          return;
+        }
+        alert("Image Uploaded");
+      } catch (err: unknown) {
+        if (axios.isAxiosError(err)) {
+          console.log(err.response?.data);
+          alert("Something went wrong");
+        }
+      } finally {
+        setPhoto(null);
+      }
+    }
+  };
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require("@/assets/images/partial-react-logo.png")}
           style={styles.reactLogo}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      }
+    >
+      <Button title="Image Picker" onPress={openImagePicker} />
+      <Button title="Open Camera Picker" onPress={openCameraPicker} />
+      <Button title="Upload Photo" onPress={uploadPhoto} />
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -65,6 +135,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
